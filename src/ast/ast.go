5fb31e91dc9b4f35a8742419ac4922d34144e54d
William Tran <wtran29@gmail.com>
2024-03-25 21:18:14 -0700

    included boolean to parser, added boolean expression test

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -189,3 +189,17 @@ func (oe *InfixExpression) String() string {
 
 	return out.String()
 }
+
+// Boolean represents booleans
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode() {}
+func (b *Boolean) TokenLiteral() string {
+	return b.Token.Literal
+}
+func (b *Boolean) String() string {
+	return b.Token.Literal
+}
