598e28ebcefb2dfbe758a091132bff4c431f7141
William Tran <wtran29@gmail.com>
2024-04-27 23:35:20 -0700

    include minus and bang opcodes to const and definition

--- src/code/code.go
+++ src/code/code.go
@@ -26,6 +26,8 @@ const (
 	OpEqual
 	OpNotEqual
 	OpGreaterThan
+	OpMinus
+	OpBang
 )
 
 // Definition defines an Opcode
@@ -46,6 +48,8 @@ var definitions = map[Opcode]*Definition{
 	OpEqual:       {"OpEqual", []int{}},
 	OpNotEqual:    {"OpNotEqual", []int{}},
 	OpGreaterThan: {"OpGreaterThan", []int{}},
+	OpMinus:       {"OpMinus", []int{}},
+	OpBang:        {"OpBang", []int{}},
 }
 
 func Lookup(op byte) (*Definition, error) {
