687ab2a2b46fb948e10616f23d36f4690fbc1fe4
William Tran <wtran29@gmail.com>
2024-04-28 01:37:02 -0700

    (compiler.go): include switch prefix expression cases for bang and minus operators, (compiler_test.go): bang and minus operator test cases

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -83,6 +83,19 @@ func (c *Compiler) Compile(node ast.Node) error {
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
 		c.emit(code.OpConstant, c.addConstant(integer))
+	case *ast.PrefixExpression:
+		err := c.Compile(node.Right)
+		if err != nil {
+			return err
+		}
+		switch node.Operator {
+		case "!":
+			c.emit(code.OpBang)
+		case "-":
+			c.emit(code.OpMinus)
+		default:
+			return fmt.Errorf("unknown operator %s", node.Operator)
+		}
 	}
 	return nil
 }
