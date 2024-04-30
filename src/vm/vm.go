6600284dc3b1ff0b3fd45369368da8fbad72a609
William Tran <wtran29@gmail.com>
2024-04-30 11:20:08 -0700

    (vm.go): add null case to executeBangOperator func and isTruthy bool case, (vm_test.go): include test for null

--- src/vm/vm.go
+++ src/vm/vm.go
@@ -110,6 +110,8 @@ func isTruthy(obj object.Object) bool {
 	switch obj := obj.(type) {
 	case *object.Boolean:
 		return obj.Value
+	case *object.Null:
+		return false
 	default:
 		return true
 	}
@@ -218,6 +220,8 @@ func (vm *VM) executeBangOperator() error {
 	case True:
 		return vm.push(False)
 	case False:
+		return vm.push(True)
+	case Null:
 		return vm.push(True)
 	default:
 		return vm.push(False)
