77d3aeeae35589813a28237096fbce99753808ef
William Tran <wtran29@gmail.com>
2024-05-01 11:49:35 -0700

    (code.go): include new opcodes GetGlobal and SetGlobal, (funckey): update and built binary

--- src/code/code.go
+++ src/code/code.go
@@ -31,6 +31,8 @@ const (
 	OpJumpNotTruthy
 	OpJump
 	OpNull
+	OpGetGlobal
+	OpSetGlobal
 )
 
 // Definition defines an Opcode
@@ -56,6 +58,8 @@ var definitions = map[Opcode]*Definition{
 	OpJumpNotTruthy: {"OpJumpNotTruthy", []int{2}},
 	OpJump:          {"OpJump", []int{2}},
 	OpNull:          {"OpNull", []int{}},
+	OpGetGlobal:     {"OpGetGlobal", []int{2}},
+	OpSetGlobal:     {"OpSetGlobal", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
