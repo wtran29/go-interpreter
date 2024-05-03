6d1b23ee490962883d0a0c0090d7586d9a7a8453
William Tran <wtran29@gmail.com>
2024-05-03 01:27:49 -0700

    include OpArray to const and definitions

--- src/code/code.go
+++ src/code/code.go
@@ -33,6 +33,7 @@ const (
 	OpNull
 	OpGetGlobal
 	OpSetGlobal
+	OpArray
 )
 
 // Definition defines an Opcode
@@ -60,6 +61,7 @@ var definitions = map[Opcode]*Definition{
 	OpNull:          {"OpNull", []int{}},
 	OpGetGlobal:     {"OpGetGlobal", []int{2}},
 	OpSetGlobal:     {"OpSetGlobal", []int{2}},
+	OpArray:         {"OpArray", []int{2}},
 }
 
 func Lookup(op byte) (*Definition, error) {
