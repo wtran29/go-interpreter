b38cefa6a1450a31509233036777f7a0594b2ba8
William Tran <wtran29@gmail.com>
2024-05-07 00:52:01 -0700

    (code.go): add OpGetLocal and OpSetLocal to consts and definitions, refactor to read uint8, (code_test.go): include tests for OpGetLocal, set up for TestInstructionsString

--- src/code/code.go
+++ src/code/code.go
@@ -39,6 +39,8 @@ const (
 	OpCall
 	OpReturnValue
 	OpReturn
+	OpGetLocal
+	OpSetLocal
 )
 
 // Definition defines an Opcode
@@ -72,6 +74,8 @@ var definitions = map[Opcode]*Definition{
 	OpCall:          {"OpCall", []int{}},        // tell the VM to start executing object.CompiledFunction on top of stack
 	OpReturnValue:   {"OpReturnValue", []int{}}, // tell the VM to return the value on top of the stack to calling context to resume execution
 	OpReturn:        {"OpReturn", []int{}},      //same as code.OpReturnValue but there is no explicit value to return but implicit vm.Null
+	OpGetLocal:      {"OpGetLocal", []int{1}},
+	OpSetLocal:      {"OpSetLocal", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
@@ -108,6 +112,8 @@ func Make(op Opcode, operands ...int) []byte {
 		switch width {
 		case 2:
 			binary.BigEndian.PutUint16(instruction[offset:], uint16(o))
+		case 1:
+			instruction[offset] = byte(o)
 		}
 		offset += width
 	}
@@ -152,11 +158,17 @@ func ReadOperands(def *Definition, ins Instructions) ([]int, int) {
 		switch width {
 		case 2:
 			operands[i] = int(ReadUint16(ins[offset:]))
+		case 1:
+			operands[i] = int(ReadUint8(ins[offset:]))
 		}
 		offset += width
 	}
 
 	return operands, offset
+}
+
+func ReadUint8(ins Instructions) uint8 {
+	return uint8(ins[0])
 }
 
 func ReadUint16(ins Instructions) uint16 {
