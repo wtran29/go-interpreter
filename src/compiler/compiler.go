e66933b891a81cfa5780f235717c312f56b5e887
William Tran <wtran29@gmail.com>
2024-05-07 13:55:45 -0700

    (compiler.go): added ranging over multiple parameters for func literal, (compiler_test.go): added more tests

--- src/compiler/compiler.go
+++ src/compiler/compiler.go
@@ -230,6 +230,11 @@ func (c *Compiler) Compile(node ast.Node) error {
 		c.emit(code.OpIndex)
 	case *ast.FunctionLiteral:
 		c.enterScope()
+
+		for _, p := range node.Parameters {
+			c.symbolTable.Define(p.Value)
+		}
+
 		err := c.Compile(node.Body)
 		if err != nil {
 			return err
@@ -263,7 +268,14 @@ func (c *Compiler) Compile(node ast.Node) error {
 		if err != nil {
 			return err
 		}
-		c.emit(code.OpCall)
+
+		for _, a := range node.Arguments {
+			err := c.Compile(a)
+			if err != nil {
+				return err
+			}
+		}
+		c.emit(code.OpCall, len(node.Arguments))
 
 	}
 	return nil
