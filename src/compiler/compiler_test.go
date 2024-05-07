fb2171241bb9740a7f5eebe730481434bda1b631
William Tran <wtran29@gmail.com>
2024-05-06 19:14:57 -0700

    included implicit return test for test function

--- src/compiler/compiler_test.go
+++ src/compiler/compiler_test.go
@@ -442,6 +442,23 @@ func TestFunctions(t *testing.T) {
 				code.Make(code.OpPop),
 			},
 		},
+		{
+			input: `fn() {5 + 10}`,
+			expectedConstants: []interface{}{
+				5,
+				10,
+				[]code.Instructions{
+					code.Make(code.OpConstant, 0),
+					code.Make(code.OpConstant, 1),
+					code.Make(code.OpAdd),
+					code.Make(code.OpReturnValue),
+				},
+			},
+			expectedInstructions: []code.Instructions{
+				code.Make(code.OpConstant, 2),
+				code.Make(code.OpPop),
+			},
+		},
 	}
 	runCompilerTests(t, tests)
 }
