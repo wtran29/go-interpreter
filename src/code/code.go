a53616f95d4a3e04177b260e60fd6897ccfda996
William Tran <wtran29@gmail.com>
2024-05-08 11:25:37 -0700

    add op free variables to const and definitions

--- src/code/code.go
+++ src/code/code.go
@@ -43,6 +43,7 @@ const (
 	OpSetLocal
 	OpGetBuiltin
 	OpClosure
+	OpGetFree
 )
 
 // Definition defines an Opcode
@@ -80,6 +81,7 @@ var definitions = map[Opcode]*Definition{
 	OpSetLocal:      {"OpSetLocal", []int{1}},
 	OpGetBuiltin:    {"OpGetBuiltin", []int{1}},
 	OpClosure:       {"OpClosure", []int{2, 1}},
+	OpGetFree:       {"OpGetFree", []int{1}},
 }
 
 func Lookup(op byte) (*Definition, error) {
